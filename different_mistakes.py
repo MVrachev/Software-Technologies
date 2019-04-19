027f980f334230a3c9a6eb81a5350402b71a5305
testUserMe2 <46818293+testUserMe2@users.noreply.github.com>
2019-04-19 11:32:35 +0300

    Update different_mistakes.py

--- different_mistakes.py
+++ different_mistakes.py
@@ -12,7 +12,7 @@ def noMatch(password):
         print("No password!")
 
 def NoMatch2(password):
-    if password == "ajklawejrkl42348swfgkg":
+    if password == "ajklawejrkl42348swfgkg":   
         print("Nice password!")
 
 def doLogin(password="blerg"):
